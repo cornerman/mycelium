1418f88a333afde6f0d34f9e1c54d0f51f26b823
johannes karoff <johannes@karoff.net>
2018-01-11 00:21:24 +0100

    update dependencies

--- project/Deps.scala
+++ project/Deps.scala
@@ -5,13 +5,13 @@ import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport._
 object Deps {
   import Def.{setting => dep}
 
-  val scalaTest = dep("org.scalatest" %%% "scalatest" % "3.0.3")
+  val scalaTest = dep("org.scalatest" %%% "scalatest" % "3.0.4")
   val scalajs = new {
     val dom = dep("org.scala-js" %%% "scalajs-dom" % "0.9.3")
   }
   val akka = new {
-    private val version = "2.4.20"
-    val http = dep("com.typesafe.akka" %% "akka-http" % "10.0.10")
+    private val version = "2.5.8"
+    val http = dep("com.typesafe.akka" %% "akka-http" % "10.0.11")
     val actor = dep("com.typesafe.akka" %% "akka-actor" % version)
     val testkit = dep("com.typesafe.akka" %% "akka-testkit" % version)
   }
