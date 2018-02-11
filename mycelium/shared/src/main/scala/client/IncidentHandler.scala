2ff659ee252ffd99de6169f0d1b2b035dc2765e0
johannes karoff <johannes@karoff.net>
2018-02-11 22:14:25 +0100

    use list event in incidenthandler

--- mycelium/shared/src/main/scala/client/IncidentHandler.scala
+++ mycelium/shared/src/main/scala/client/IncidentHandler.scala
@@ -3,5 +3,5 @@ package mycelium.client
 class IncidentHandler[Event] {
   def onConnect(): Unit = {}
   def onClose(): Unit = {}
-  def onEvents(events: Seq[Event]): Unit = {}
+  def onEvents(events: List[Event]): Unit = {}
 }
