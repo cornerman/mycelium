be0d69cc5a357b06ab402c0e8105005f04c70a5b
johannes karoff <johannes@karoff.net>
2018-03-28 22:42:55 +0200

    update chameleon

--- project/Deps.scala
+++ project/Deps.scala
@@ -17,7 +17,7 @@ object Deps {
     val testkit = dep("com.typesafe.akka" %% "akka-testkit" % version)
   }
   val okhttp = dep("com.squareup.okhttp3" % "okhttp" % "3.9.1")
-  val chameleon = dep("com.github.cornerman.chameleon" %%% "chameleon" % "1bbc84c")
+  val chameleon = dep("com.github.cornerman.chameleon" %%% "chameleon" % "7dacc9f")
   val boopickle = dep("io.suzaku" %%% "boopickle" % "1.3.0")
   val scribe = dep("com.outr" %%% "scribe" % "2.3.0")
 }
