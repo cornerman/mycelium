6c1d970e716f39aa2b5a60756e94a5e9f0fce610
johannes karoff <johannes@karoff.net>
2021-04-24 15:20:20 +0200

    update chameleon

--- project/Deps.scala
+++ project/Deps.scala
@@ -11,12 +11,12 @@ object Deps {
   }
   val akka = new {
     private val version = "2.6.6"
-    val http = dep("com.typesafe.akka" %% "akka-http" % "10.2.1")
-    val stream = dep("com.typesafe.akka" %% "akka-stream" % version)
-    val actor = dep("com.typesafe.akka" %% "akka-actor" % version)
-    val testkit = dep("com.typesafe.akka" %% "akka-testkit" % version)
+    val http            = dep("com.typesafe.akka" %% "akka-http" % "10.2.1")
+    val stream          = dep("com.typesafe.akka" %% "akka-stream" % version)
+    val actor           = dep("com.typesafe.akka" %% "akka-actor" % version)
+    val testkit         = dep("com.typesafe.akka" %% "akka-testkit" % version)
   }
-  val chameleon = dep("com.github.cornerman" %%% "chameleon" % "0.3.0")
+  val chameleon = dep("com.github.cornerman.chameleon" %%% "chameleon" % "b19ace1")
   val boopickle = dep("io.suzaku" %%% "boopickle" % "1.3.3")
-  val scribe = dep("com.outr" %%% "scribe" % "2.7.13")
+  val scribe    = dep("com.outr" %%% "scribe" % "2.7.13")
 }
