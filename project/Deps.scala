cac3c520af26cd9c39d8a15b4d0ee1d24b7e792e
Scala Steward <me@scala-steward.org>
2021-08-26 08:15:28 +0200

    Update scalajs-dom to 1.2.0

--- project/Deps.scala
+++ project/Deps.scala
@@ -7,7 +7,7 @@ object Deps {
 
   val scalaTest = dep("org.scalatest" %%% "scalatest" % "3.2.2")
   val scalajs = new {
-    val dom = dep("org.scala-js" %%% "scalajs-dom" % "1.1.0")
+    val dom = dep("org.scala-js" %%% "scalajs-dom" % "1.2.0")
   }
   val akka = new {
     private val version = "2.6.6"
