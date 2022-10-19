d8614bf10f920c838a979bd12b154cf831715f9b
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-10-19 18:08:39 +0200

    Update scribe to 3.10.4 (#195)

--- project/Deps.scala
+++ project/Deps.scala
@@ -18,5 +18,5 @@ object Deps {
   }
   val chameleon = dep("com.github.cornerman" %%% "chameleon" % "0.3.5")
   val boopickle = dep("io.suzaku" %%% "boopickle" % "1.4.0")
-  val scribe    = dep("com.outr" %%% "scribe" % "3.10.3")
+  val scribe    = dep("com.outr" %%% "scribe" % "3.10.4")
 }
