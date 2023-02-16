9518232d9f64e431688565cf02e74223c6f09c81
Scala Steward <scala_steward@virtuslab.com>
2023-02-16 15:00:49 +0000

    Update scalajs-dom to 2.4.0

--- project/Deps.scala
+++ project/Deps.scala
@@ -7,7 +7,7 @@ object Deps {
 
   val scalaTest = dep("org.scalatest" %%% "scalatest" % "3.2.15")
   val scalajs = new {
-    val dom = dep("org.scala-js" %%% "scalajs-dom" % "2.3.0")
+    val dom = dep("org.scala-js" %%% "scalajs-dom" % "2.4.0")
   }
   val akka = new {
     private val version = "2.6.16"
