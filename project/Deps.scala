6df9ffd35ba3b4087b8c8671e218acbae2018f53
Scala Steward <scala_steward@virtuslab.com>
2024-06-24 12:47:11 +0000

    Update scalatest to 3.2.19

--- project/Deps.scala
+++ project/Deps.scala
@@ -5,7 +5,7 @@ import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
 object Deps {
   import Def.{setting => dep}
 
-  val scalaTest = dep("org.scalatest" %%% "scalatest" % "3.2.18")
+  val scalaTest = dep("org.scalatest" %%% "scalatest" % "3.2.19")
   val scalajs = new {
     val dom = dep("org.scala-js" %%% "scalajs-dom" % "2.8.0")
   }
